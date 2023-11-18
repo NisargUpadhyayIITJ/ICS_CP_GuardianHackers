b8600355e8859bc405ede4d0e609f8a3e648dad5
NisargUpadhyayIITJ <141734373+NisargUpadhyayIITJ@users.noreply.github.com>
2023-11-18 09:50:08 +0530

    Add files via upload

--- ICfinalpro.c
+++ ICfinalpro.c
@@ -1,172 +1,238 @@
-#include<stdio.h>
-#include<time.h>
-#include<string.h>
-#include<ctype.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int prev_check(char a[]);
 
 int checking_strength(char a[])
 {
-    int count =0,star= 0;
-    for(int i=0;i<10;i++)
+    int count = 0, star = 0;
+    for (int i = 0; i < 10; i++)
     {
-        if(isdigit(a[i]))
+        if (isdigit(a[i]))
         {
-            count ++;
+            count++;
         }
     }
-    if (count ==10)
+    if (count == 10)
     {
         printf("50%% strength\n");
         return 0;
     }
-    int b =0;
-    for (int i =0;i<10;i++)
+    int b = 0;
+    for (int i = 0; i < 10; i++)
     {
         if (isdigit(a[i]) || islower(a[i]))
         {
-            if(islower(a[i]))
+            if (islower(a[i]))
             {
                 star++;
             }
             b++;
         }
     }
-    if(b ==10 && star>=1)
+    if (b == 10 && star >= 1)
     {
         printf("65%% strength\n");
         return 0;
     }
-    int c=0,d=0,e=0;
-    for(int i=0;i<10;i++)
+    int c = 0, d = 0, e = 0;
+    for (int i = 0; i < 10; i++)
     {
-        if(isdigit(a[i]) ||islower(a[i])||isalpha(a[i]))
+        if (isdigit(a[i]) || islower(a[i]) || isalpha(a[i]))
         {
-            c ++;
-            if(islower(a[i]))
+            c++;
+            if (islower(a[i]))
             {
                 d++;
             }
-            if(isalpha(a[i]))
+            if (isalpha(a[i]))
             {
                 e++;
             }
         }
     }
-    if(c ==10&& d>=1&& e>=1)
+    if (c == 10 && d >= 1 && e >= 1)
     {
         printf("75%% strength\n");
         return 0;
     }
-    int f=0,g=0,h=0,k=0;
-    for (int i=0;i<10;i++)
+    int f = 0, g = 0, h = 0, k = 0;
+    for (int i = 0; i < 10; i++)
     {
-        if(isdigit(a[i]) ||islower(a[i])||isalpha(a[i])||a[i]=='?'||a[i]=='-'||a[i]=='+'||a[i]=='=')
+        if (isdigit(a[i]) || islower(a[i]) || isalpha(a[i]) || a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=')
         {
-            f ++;
-            if(islower(a[i]))
+            f++;
+            if (islower(a[i]))
             {
                 g++;
             }
-            if(isalpha(a[i]))
+            if (isalpha(a[i]))
             {
                 h++;
             }
-            if(a[i]=='?'||a[i]=='-'||a[i]=='+'||a[i]=='=')
+            if (a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=')
             {
                 k++;
             }
         }
     }
-    if(f ==10 && g>=1&&h>=1&&k>=1)
+    if (f == 10 && g >= 1 && h >= 1 && k >= 1)
     {
         printf("85%% strength\n");
         return 0;
     }
-    int l=0,m=0,n=0,o=0,p=0;
-    for(int i=0;i<10;i++)
+    int l = 0, m = 0, n = 0, o = 0, p = 0;
+    for (int i = 0; i < 10; i++)
     {
-        if(isdigit(a[i]) ||islower(a[i])||isalpha(a[i])||a[i]=='?'||a[i]=='-'||a[i]=='+'||a[i]=='='||a[i]=='!'||a[i]=='@'||a[i]=='#'||a[i]=='$')
+        if (isdigit(a[i]) || islower(a[i]) || isalpha(a[i]) || a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '!' || a[i] == '@' || a[i] == '#' || a[i] == '$')
         {
-            l ++;
-            if(islower(a[i]))
+            l++;
+            if (islower(a[i]))
             {
                 m++;
             }
-            if(isalpha(a[i]))
+            if (isalpha(a[i]))
             {
                 n++;
             }
-            if(a[i]=='?'||a[i]=='-'||a[i]=='+'||a[i]=='='||a[i]=='!'||a[i]=='@'||a[i]=='#'||a[i]=='$')
+            if (a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '!' || a[i] == '@' || a[i] == '#' || a[i] == '$')
             {
                 o++;
             }
         }
     }
-    if(l ==10 && m>=2&&n>=2&&o>=2)
+    if (l == 10 && m >= 2 && n >= 2 && o >= 2)
     {
-        printf("100%% strength\n");
-        return 1;
+        int abcdefghij = prev_check(a);
+        if (abcdefghij != 3)
+        {
+            printf("100%% strength\n");
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
     }
     else
     {
         printf("<50%% strength\n");
         return 2;
     }
-    return 0;
 }
 
 void randomf(char a[])
 {
-    char lower[]="abcdefghijklmnopqrstuvwxyz";
-    char upper[]="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char special[]="?+-!@#$";
-    char number[]="0123456789";
+    char lower[] = "abcdefghijklmnopqrstuvwxyz";
+    char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char special[] = "?+-!@#$";
+    char number[] = "0123456789";
 
     srand(time(NULL));
 
-    
-        a[0]=lower[rand()%26];
-        a[1]=special[rand()%7];
-        a[2]=upper[rand()%26];
-        a[3]=number[rand()%10];
-        a[4]=lower[rand()%26];
-        a[5]=special[rand()%7];
-        a[6]=upper[rand()%26];
-        a[7]=number[rand()%10];
-        a[8]=lower[rand()%26];
-        a[9]=special[rand()%7];
+    a[0] = lower[rand() % 26];
+    a[1] = special[rand() % 7];
+    a[2] = upper[rand() % 26];
+    a[3] = number[rand() % 10];
+    a[4] = lower[rand() % 26];
+    a[5] = special[rand() % 7];
+    a[6] = upper[rand() % 26];
+    a[7] = number[rand() % 10];
+    a[8] = lower[rand() % 26];
+    a[9] = special[rand() % 7];
 
-    
-    int i = rand()%10;
-    int j=rand()%10;
-    char temp = a[i] ;
-    a[i]=a[j];
-    a[j]=temp;
-
-
-    printf("%s\n",a);
-    
+    int i = rand() % 10;
+    int j = rand() % 10;
+    char temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+   
 }
 
+int prev_check(char a[])
+{
+    FILE *fp;
+    fp = fopen("prev_pass.txt", "r+");
+    if (fp == NULL)
+    {
+        printf("Unable to open the file\n");
+        return 3;
+    }
+    else
+    {
+        int found = 0;
+        char temp_str[11];
+        while (!feof(fp))
+        {
+            fscanf(fp, "%s", temp_str);
+            if (strcmp(a, temp_str) == 0)
+            {
+                printf("The password generated has been already generated previously\n");
+                found = 1;
+                return 3;
+            }
+        }
+        if (!found)
+        {
+            fprintf(fp, "%s\n", a);
+            return 15;
+        }
+        fclose(fp);
+    }
+    return 34;
+}
 
-int main ()
+int main()
 {
     char a[10];
-    scanf("%s",a);
+    printf("Enter Password: ");
+    scanf("%s", a);
     int b;
     b = checking_strength(a);
-    if(b==0||b==2)
+    if (b == 0 || b == 2)
     {
-        printf("New suggested password: ");
         randomf(a);
+        int yuiop = prev_check(a);
+        if (yuiop == 15)
+        {
+            printf("New suggested password: ");
+            printf("%s\n", a);
+        }
+        else
+        {
+            randomf(a);
+            printf("New suggested password: ");
+            printf("%s\n", a);
+        }
     }
-    
     else
     {
         printf("Your password is strong.");
     }
-    
+    int play;
+    printf("Enter 1 if you want to regenerate password else press 0 : ");
+    scanf("%d", &play);
+    while (play != 0)
+    {
 
-
+        randomf(a);
+        int yui = prev_check(a);
+        if (yui == 15)
+        {
+            printf("New suggested password: ");
+            printf("%s\n", a);
+        }
+        else
+        {
+            randomf(a);
+            printf("New suggested password: ");
+            printf("%s\n", a);
+        }
+        printf("Enter 1 if you want to regenerate password else press 0 : ");
+        scanf("%d", &play);
+    }
     return 0;
 }
