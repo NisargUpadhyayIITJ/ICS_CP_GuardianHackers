8f53b83e99602a2ffdef20af71c9c6c842b6a212
NisargUpadhyayIITJ <141734373+NisargUpadhyayIITJ@users.noreply.github.com>
2023-12-02 21:24:47 +0530

    Add files via upload

--- ICfinalpro.c
+++ ICfinalpro.c
@@ -1,3 +1,22 @@
+/*Create a tool in C that crafts a password, the complexity of which is determined by user-defined
+criteria. The produced password should be 10 characters in length, and its strength is gauged based
+on specific guidelines. Should the generated password fail to meet the maximum robustness
+standard, the program will refine and represent a password that embodies a full strength score.
+
+50% strength: The password comprises only numbers.
+
+65% strength: The password consists of numbers and lowercase alphabets.
+
+75% strength: The password seamlessly blends numbers, lowercase, and uppercase alphabets.
+
+85% strength: Introducing special characters into the mix, the password now has numbers, both
+lowercase and uppercase alphabets, and select special characters (?,+,=,-).
+
+100% strength: Achieving pinnacle strength, the password incorporates numbers, both cases of
+alphabets, and an expansive range of special characters (?,+,=,-,!,@,#,$). Furthermore, each
+character type should be represented at least twice.*/
+
+
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
@@ -10,143 +29,165 @@
 int prev_check(char a[]);
 
 // A function to check the strength of the password using a specific criteria
-int checking_strength(char a[]);
+int checking_strength(char a[], int size);
 
 // This function generates a random password that meets the criteria for 100% strength
-void randomf(char a[]);
+void randomf(char a[],int n);
 
 
 
-int checking_strength(char a[])
+int checking_strength(char a[],int size)
 {
+    int digit = 0, small_alphabet = 0, big_alphabet = 0, special = 0;
     // Criteria for different strength levels
     // The function returns a strength level and prints a corresponding message
 
     // Check for 50% strength
-    int count = 0, star = 0;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < size; i++)
     {
         if (isdigit(a[i]))
         {
-            count++;
+            digit++;
+        }
+        if(islower(a[i]) || isupper(a[i])|| a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '@' || a[i] == '!' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
+        {
+            special++;
         }
     }
-    if (count == 10)
+    if (digit >= 10 && special ==0)
     {
         printf("50%% strength\n");
         return 0;
     }
-
+    digit = 0;
+    special = 0;
     // Check for 65% strength
-    int b = 0,sun=0;
-    for (int i = 0; i < 10; i++)
+    int b = 0;
+    for (int i = 0; i < size; i++)
     {
-        if (isdigit(a[i]) || islower(a[i]))
+        if (isdigit(a[i]) || islower(a[i]) || isupper(a[i]) || a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '@' || a[i] == '!' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
         {
             if (islower(a[i]))
             {
-                star++;
+                small_alphabet++;
             }
             if (isdigit(a[i]))
             {
-                sun++;
+                digit++;
+            }
+            if(isupper(a[i]) || a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '@' || a[i] == '!' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
+            {
+                special++; 
             }
             b++;
         }
     }
-    if (b == 10 && star >= 1 && sun>=1)
+    if (b >= 10 && small_alphabet >= 1 && digit >=1 && special == 0)
     {
         printf("65%% strength\n");
         return 0;
     }
-
+    small_alphabet = 0;
+    digit = 0;
+    special = 0;
     // Check for 75% strength
-    int c = 0, d = 0, e = 0,fr=0;
-    for (int i = 0; i < 10; i++)
+    int c = 0;
+    for (int i = 0; i < size; i++)
     {
-        if (isdigit(a[i]) || islower(a[i]) || isupper(a[i]))
+        if (isdigit(a[i]) || islower(a[i]) || isupper(a[i]) || a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '@' || a[i] == '!' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
         {
             c++;
             if (islower(a[i]))
             {
-                d++;
+                small_alphabet++;
             }
             if (isupper(a[i]))
             {
-                e++;
+                big_alphabet++;
             }
             if (isdigit(a[i]))
             {
-                fr++;
+                digit++;
+            }
+            if(a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '@' || a[i] == '!' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
+            {
+                special++;
             }
         }
     }
 
-    if (c == 10 && d >= 1 && e >= 1 && fr>=1)
+    if (c >= 10 && small_alphabet >= 1 && big_alphabet >= 1 && digit >=1 && special == 0)
     {
         printf("75%% strength\n");
         return 0;
     }
-
+    small_alphabet = 0;
+    digit = 0;
+    special = 0;
+    big_alphabet = 0;
     // Check for 85% strength
-    int f = 0, g = 0, h = 0, hr = 0, k = 0;
-    for (int i = 0; i < 10; i++)
+    int f = 0;
+    for (int i = 0; i < size; i++)
     {
         if (isdigit(a[i]) || islower(a[i]) || isupper(a[i]) || a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '@' || a[i] == '!' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
         {
             f++;
             if (islower(a[i]))
             {
-                g++;
+                small_alphabet++;
             }
             if (isupper(a[i]))
             {
-                h++;
+                big_alphabet++;
             }
             if (a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '@' || a[i] == '!' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
             {
-                k++;
+                special++;
             }
             if (isdigit(a[i]))
             {
-                hr++;
+                digit++;
             }
         }
     }
 
-    if (f == 10 && (g < 2 || h < 2 || hr < 2 || k < 2))
+    if (f >= 10 && (small_alphabet < 2 || big_alphabet < 2 || digit < 2 ||special < 2) && (big_alphabet > 0 && small_alphabet > 0 && special > 0 && digit > 0))
     {
         printf("85%% strength\n");
         return 0;
     }
+    small_alphabet = 0;
+    digit = 0;
+    special = 0;
+    big_alphabet = 0;
 
     // Check for 100% strength
-    int l = 0, m = 0, n = 0, o = 0,p=0;
-    for (int i = 0; i < 10; i++)
+    int l = 0;
+    for (int i = 0; i < size; i++)
     {
         if (isdigit(a[i]) || islower(a[i]) || isupper(a[i]) || a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '!' || a[i] == '@' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
         {
             l++;
             if (islower(a[i]))
             {
-                m++;
+                small_alphabet++;
             }
             if (isupper(a[i]))
             {
-                n++;
+                big_alphabet++;
             }
             if (a[i] == '?' || a[i] == '-' || a[i] == '+' || a[i] == '=' || a[i] == '!' || a[i] == '@' || a[i] == '#' || a[i] == '$' || a[i] == '^' || a[i] == '&' || a[i] == '*')
             {
-                o++;
+                special++;
             }
             if (isdigit(a[i]))
             {
-                p++;
+                digit++;
             }
         }
     }
 
-    if (l == 10 && m >= 2 && n >= 2 && o >= 2 && p >= 2)
+    if (l >= 10 && small_alphabet >= 2 && big_alphabet >= 2 && special >= 2 && digit >= 2)
     {
         int q = prev_check(a);
         if (q != 3)
@@ -169,32 +210,40 @@ int checking_strength(char a[])
 }
 
 
-void randomf(char a[])
+void randomf(char a[],int n)
 {
     char lower[] = "abcdefghijklmnopqrstuvwxyz";
     char upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char special[] = "?+-!@#$";
+    char special[] = "?+-!@#$^&*";
     char number[] = "0123456789";
 
     srand(time(NULL));
 
     a[0] = lower[rand() % 26];
-    a[1] = special[rand() % 7];
+    a[1] = special[rand() % 10];
     a[2] = upper[rand() % 26];
     a[3] = number[rand() % 10];
     a[4] = lower[rand() % 26];
-    a[5] = special[rand() % 7];
+    a[5] = special[rand() % 10];
     a[6] = upper[rand() % 26];
     a[7] = number[rand() % 10];
     a[8] = lower[rand() % 26];
-    a[9] = special[rand() % 7];
+    a[9] = special[rand() % 10];
+    for(int k=10;k<n;k++)
+    {
+        a[k]='\0';
+    }
 
     // this swaps any two characters randomly in the string so that the password is completely random
-    int i = rand() % 10;
-    int j = rand() % 10;
-    char temp = a[i];
-    a[i] = a[j];
-    a[j] = temp;
+    for(int k=0;k<4;k++)
+    {
+        int i = rand() % 10;
+        int j = rand() % 10;
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    
 }
 
 
@@ -210,7 +259,7 @@ int prev_check(char a[])
     else
     {
         int found = 0;
-        char temp_str[11];
+        char temp_str[100];
         while (!feof(fp))
         {
             // Check f the generated password has been generated previously
@@ -229,7 +278,7 @@ int prev_check(char a[])
             fprintf(fp, "%s\n", a);
             return 15;
         }
-
+  
         // Close the file
         fclose(fp);
     }
@@ -238,17 +287,25 @@ int prev_check(char a[])
 
 int main()
 {
-    char a[10];
+    char a[100];
     int play = 1;
     int b;
     while (play == 1)
     {
         printf("Enter Password: ");
         scanf("%s", a);
-        b = checking_strength(a);
+        int size = strlen(a);
+        while(size < 10)
+        {
+            printf("Password should contains atleast 10 characters\n");
+            printf("Re-enter password: ");
+            scanf("%s",a);
+            size = strlen(a);
+        }
+        b = checking_strength(a,size);
         if (b == 0 || b == 2)
         {
-            randomf(a);
+            randomf(a,size);
             int y = prev_check(a);
             if (y == 15)
             {
@@ -257,7 +314,7 @@ int main()
             }
             else
             {
-                randomf(a);
+                randomf(a,size);
                 printf("New suggested password: ");
                 printf("%s\n", a);
             }
@@ -269,13 +326,13 @@ int main()
 
         // Loop to generate more than one password
 
-        printf("Enter 1 if you want to re-enter password\nEnter 2 if you want to re-generate the password\nEnter 0 to exit : ");
+        printf("Enter 1 if you want to re-enter password\nEnter 2 if you want to re-generate the password\nElse any other number to exit: ");
 
         scanf("%d", &play);
 
         while (play == 2)
         {
-            randomf(a);
+            randomf(a,size);
             int u = prev_check(a);
             if (u == 15)
             {
@@ -284,13 +341,15 @@ int main()
             }
             else
             {
-                randomf(a);
+                randomf(a,size);
+
                 printf("New suggested password: ");
+
                 printf("%s\n", a);
             }
             printf("Enter 1 if you want to re-enter password\n");
             printf("Enter 2 if you want to re-generate the password\n");
-            printf("Enter 0 : ");
+            printf("Else any other number to exit: ");
             scanf("%d", &play);
         }
     }
